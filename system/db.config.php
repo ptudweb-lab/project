10726b0047170ac371cee8f3493b7b4822da55dc
arituan <arituan@gmail.com>
2018-02-12 10:32:54 +0700

    Info Database

---
+++ system/db.config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+* Name: FES (Fast Ecommerce Safety)
+* Author: github.com/ptudweb-lab/project
+* Version: VERSION.txt
+*/
+defined('_IN_FES') or die('Error: restricted access');
+
+$db = [
+    'driver' => 'mysql', //driver SQL as: mysql, sqlite,...
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'dbname' => '' //database name
+];
