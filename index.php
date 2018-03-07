e4027ab6ed9b36af0c4880b2534865c36b36cd8c
arituan <arituan@gmail.com>
2018-03-07 09:55:01 +0700

    Update template weeks 4

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+* Name: FES (Fast Ecommerce Safety)
+* Author: github.com/ptudweb-lab/project
+* Version: VERSION.txt
+*/
+define('_IN_FES', 1);
+
+//test connect db
+//require_once('system/db.php');
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script language="javascript"
+    <title>Cửa hàng FS</title>
+</head>
+<body>
+    
+</body>
+</html>
