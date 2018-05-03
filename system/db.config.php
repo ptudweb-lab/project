2fe8869cbbac7ee0b085e30390fde86cfbc018e8
Hoang Tuan <arituan@gmail.com>
2018-05-03 20:19:27 +0700

    update to apply to openshift

--- system/db.config.php
+++ system/db.config.php
@@ -9,8 +9,8 @@ defined('_IN_FS') or die('Error: restricted access');
 
 $dbconf = [
     'driver' => 'mysql', //driver SQL as: mysql, sqlite,...
-    'host' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'dbname' => 'fs' //database name
+    'host' => '127.0.0.1',
+    'username' => 'tuanlh',
+    'password' => '123456789',
+    'dbname' => 'sampledb' //database name
 ];
