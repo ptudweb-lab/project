0cfbe24a610dc214a6f2c177850006f0195d8970
arituan <arituan@gmail.com>
2018-06-05 22:41:39 +0700

    Add privacy description

---
+++ privacy.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Name: FS
+ * Author: github.com/ptudweb-lab/project
+ * Version: VERSION.txt
+ */
+
+define('_IN_FS', 1);
+include('system/core.php');
+
+$tpl->display('privacy.html');
+
